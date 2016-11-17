cff508b3eef5395b407fc10c1ad8112a332778e7
qdree <ta4ckpih@gmail.com>
2016-11-17 15:08:49 +0200

    pyGame examples

--- pygame_test/fonttext.py
+++ pygame_test/fonttext.py
@@ -15,29 +15,22 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 128)
 
-fontObj = pygame.font.Font('freesansbold.ttf', 40)
-textSurfaceObj1 = fontObj.render('PASSWORD', True, WHITE, BLACK)
-textSurfaceObj2 = fontObj.render('CORRECT!', True, WHITE, BLACK)
-textRectObj1 = textSurfaceObj1.get_rect()
-textRectObj1.center = (1280 / 2.0, height / 2.0)
-textRectObj2 = textSurfaceObj2.get_rect()
-textRectObj2.center = (1280 / 2.0 - 20, height / 2.0)
 while True: # main game loop
 	DISPLAYSURF.fill(BLACK)
 
 	for event in pygame.event.get():
+		if event.type == KEYDOWN:
+			if event.key == K_q: 
+				pygame.quit()
+				quit()
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
-	pygame.display.update()
 
-	input_password = raw_input()
-	while not input_password == password:
-		DISPLAYSURF.blit(textSurfaceObj1, textRectObj1)
-		input_password = raw_input()
-	print ("Correct password!")
-	if input_password == password:
-		DISPLAYSURF.blit(textSurfaceObj2, textRectObj2)
-		time.sleep(2)
-		quit()
-	
+		if event.type is KEYDOWN and event.key == K_w:
+			pygame.display.set_mode((1280, height))
+		if event.type is KEYDOWN and event.key == K_f:
+			pygame.display.set_mode((1280, height), FULLSCREEN)
+
+	pygame.display.update()
+	# time.sleep(1)
